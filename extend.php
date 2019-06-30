f4ff80c269afdd7c8cb180aa3c9ea6a0cd257431
parsonswy <parsonswy@gmail.com>
2019-06-30 17:02:14 -0400

    Initial Committ

---
+++ extend.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use pcnnet\discord\DiscordAuthController;
+use Flarum\Extend;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
+
+    new Extend\Locales(__DIR__ . '/locale')
+];
